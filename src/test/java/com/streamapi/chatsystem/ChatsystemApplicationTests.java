1543dda6cbe905df94a849c9571e0844b1f0723f
Ravindra Singh <ravindrasingh@Ravindras-MacBook-Pro.local>
2024-10-25 12:51:22 +0530

    Added initial files

---
+++ src/test/java/com/streamapi/chatsystem/ChatsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamapi.chatsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
